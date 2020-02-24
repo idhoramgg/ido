b2604d12dc6c629455eed40778582f181b878e7f
Ridho Abdul Majid <ridhoabdulmajid@gmail.com>
2020-02-24 14:30:44 +0700

    dumdum

--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
+import About from './components/About'
+import Contact from './components/Contact'
 
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
+
+const Main = withRouter(() => {
+    return (
+      <div>
+        <Route exact path="/" component={App} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact}/>
+      </div>
+    );
+  });
 
 ReactDOM.render(
 <Router>
