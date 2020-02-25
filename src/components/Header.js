206b1dd37f49b8b592c1fb8cdc8d57656e28b5ab
Ridho Abdul Majid <ridhoabdulmajid@gmail.com>
2020-02-25 15:22:07 +0700

    text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,44 @@
 import React from "react";
 import image from "../assets/images/bgido.png";
-import pdff from '../assets/files/file.pdf';
+import pdff from "../assets/files/file.pdf";
+import Typewriter from 'typewriter-effect';
+
 
 export const Header = () => {
+
   const imagetop = {
     backgroundImage: `url(${image})`,
     height: "300px",
     maxWidth: "100%",
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
-    backgroundSize: 'cover'
+    backgroundSize: "cover"
   };
 
   const hero = {
-      width: '350px',
-      height: '100px',
-      position: 'relative',
-      top: '30%'
-  }
-  const h3style = {
-      color: 'white',
-      fontSize: '26px',
-      fontFamily: 'Raleway',
-  }
+    width: "350px",
+    height: "100px",
+    position: "relative",
+    top: "30%",
+    color: 'white',
+    fontSize: '26px',
+    fontFamily: 'Raleway'
+  };
+ 
   return (
     <div style={imagetop}>
       <div style={hero}>
-            <h3 style={h3style}>Ridho Abdul Majid</h3>
-            <a className="btn btn-warning" href={pdff} download> Download CV</a>
+        <Typewriter
+          options={{
+            strings: ["Ridho Abdul Majid"],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+        <a className="btn btn-warning" href={pdff} download>
+          {" "}
+          Download CV
+        </a>
       </div>
     </div>
   );
